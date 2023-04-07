3be9bc92b417b32d42929328d22b1d88c4e383c6
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-07 19:08:13 +0200

    Komponencek létrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './Admin/admin/admin.component';
+import { LoginComponent } from './Admin/login/login.component';
+import { RegisterComponent } from './Admin/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
