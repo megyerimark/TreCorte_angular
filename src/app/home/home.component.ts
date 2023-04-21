38c850f92e667b830c3f8f110065f6c8e927da52
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-21 09:53:11 +0200

    Vissza számlálás sikeres

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 
@@ -8,7 +8,8 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit{
+
 
 
   homer= false;
@@ -19,6 +20,38 @@ export class HomeComponent {
  show(){
 
  }
- 
+
+
+
+
+  private countDate!: number;
+public day!: number;
+public hour!: number;
+public minute!: number;
+public second!: number;
+
+ngOnInit(): void {
+  this.countDate = new Date('apr 28, 2023 00:00:00').getTime();
+  setInterval(() => {
+    this.countDown();
+  }, 1000);
+}
+
+private countDown() {
+  let now = new Date().getTime();
+  let gap = this.countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  this.day = Math.floor(gap / day);
+  this.hour = Math.floor((gap % day) / hour);
+  this.minute = Math.floor((gap % hour) / minute);
+  this.second = Math.floor((gap % minute) / second);
+}
 
 }
+
+
