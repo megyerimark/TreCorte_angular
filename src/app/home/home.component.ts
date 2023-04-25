c8f1a1b14408d364956fc64779213d07c0fbfd85
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-25 10:12:22 +0200

    Menu index, reservation index felesleges kikommentelve, a Kategória törlés sikeres

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import Swal from 'sweetalert2';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { ReservationService } from '../Admin/Shared/reservation.service';
+import { MenuService } from '../Admin/Shared/menu.service';
 
 
 
@@ -10,19 +17,21 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class HomeComponent  implements OnInit{
 
+  currentUser: any;
+  menus: any;
 
 
-  homer= false;
+  form!:FormGroup;
+reservations: any;
+  constructor(
+    private reservationServ: ReservationService,
+    private router:Router,
+    private toastr: ToastrService,
+    private http:HttpClient,
+    private menuservice: MenuService,
+    private formBuild:FormBuilder){
 
-  home(){
-  this.homer = !this.homer;
-  }
- show(){
-
- }
-
-
-
+    }
 
   private countDate!: number;
 public day!: number;
@@ -31,11 +40,28 @@ public minute!: number;
 public second!: number;
 
 ngOnInit(): void {
-  this.countDate = new Date('apr 28, 2023 00:00:00').getTime();
+  this.countDate = new Date('jun 2, 2023 00:00:00').getTime();
   setInterval(() => {
     this.countDown();
   }, 1000);
+
+  this.index();
+
+
+
+
+  this.form = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl(''),
+    tel_number: new FormControl(''),
+    res_date: new FormControl(''),
+    guest_number: new FormControl('')
+  });
 }
+
+
+
 
 private countDown() {
   let now = new Date().getTime();
@@ -52,6 +78,57 @@ private countDown() {
   this.second = Math.floor((gap % minute) / second);
 }
 
+index(){
+
+  this.menuservice.index().subscribe({
+    next:res=>{
+      this.menus = res;
+      console.log('Megyeri Márk Máté');
+    }
+  });
+}
+
+
+
+
+
+
+success!:any;
+    reservation(){
+
+      let  first_name = this.form.value.first_name;
+      let  last_name = this.form.value.last_name;
+      let  email = this.form.value.email;
+      let  tel_number = this.form.value.tel_number;
+      let  res_date = this.form.value.res_date;
+      let  guest_number = this.form.value.guest_number;
+
+      this.reservationServ.store(first_name,last_name,email,tel_number,
+        res_date,guest_number).subscribe({
+          next : data =>{
+            Swal.fire(
+            'Sikeres Foglalás:' , data.data.first_name +'\n' + data.data.last_name,'success');
+            this.form.setValue({
+              first_name: '',
+              last_name: '',
+              email: '',
+              tel_number: '',
+              res_date: '',
+              guest_number: ''
+            });
+            
+            
+      
+          },
+          error:err =>{
+            this.toastr.error('Hiba! A foglalás sikertelen!');
+
+          }
+        });
+
+      
+      
+      }
 }
 
 
